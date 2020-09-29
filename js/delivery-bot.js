b1315abed22e3bb0026f29166cd3c162b2ed623d
kaile <51025570+Qikaile@users.noreply.github.com>
2020-09-29 19:34:29 +0800

    Update delivery-bot.js

--- js/delivery-bot.js
+++ js/delivery-bot.js
@@ -1,15 +1,15 @@
-﻿var botui = new BotUI("botui-app");
+var botui = new BotUI("botui-app");
  
-botui.message.bot({
+botui.message.add ({
     delay: 200,
     content: "Hi, 你好! 👋
 }).then(function() {
-    return botui.message.bot({
+    return botui.message.add({
         delay: 1000,
         content: "我是这个网站的博主,一个可爱的蓝孩子~😊"
     })
 }).then(function() {
-    return botui.message.bot({
+    return botui.message.add({
         delay: 1500,
         content: "我在业余时间玩一下网站！😎"
     })
@@ -30,7 +30,7 @@ botui.message.bot({
         other()
     }
     if (res.value == "gg") {
-        return botui.message.bot({
+        return botui.message.add({
             delay: 1500,
             content: "![告辞](https://cdn.jsdelivr.net/gh/Qikaile/cdn/img/Hd33fe77ff56f48e4b988d6c4f9091551S.jpg)"
         })
@@ -38,21 +38,21 @@ botui.message.bot({
 });
  
 var other = function() {
-    botui.message.bot({
+    botui.message.add({
         delay: 1500,
         content: "我现在是机械工程专业的研究生"
     }).then(function() {
-        return botui.message.bot({
+        return botui.message.add({
             delay: 1500,
             content: "在寒假的时候就想着拥有属于自己的博客,并且对计算机方面有着格外感兴趣。"
         })
     }).then(function() {
-        return botui.message.bot({
+        return botui.message.add({
             delay: 2000,
             content: "略懂HTML/CSS/JavaScript，偶尔也折腾PHP、Python"
         })
     }).then(function() {
-        return botui.message.bot({
+        return botui.message.add({
             delay: 1800,
             content: "喜欢折腾，热爱折腾，目前正在计算机的道上探索中"
         })
@@ -65,12 +65,12 @@ var other = function() {
             }]
         })
     }).then(function(res) {
-        return botui.message.bot({
+        return botui.message.add({
             delay: 2000,
             content: "emmm..这应该是我在上大学期间给自己QQ名的一个简写吧！"
         })
     }).then(function(res) {
-        return botui.message.bot({
+        return botui.message.add({
             delay: 1500,
             content: "那么，仔细看看我的博客吧？😇 "
         })
